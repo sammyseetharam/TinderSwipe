7288d959e245fdb8fa20d7f7cdbf5972055e637c
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-11-07 11:03:01 -0600

    connecting tutorial to song cards

--- app/src/main/java/com/jabirdeveloper/tinderswipe/rightTutorial.java
+++ app/src/main/java/com/jabirdeveloper/tinderswipe/rightTutorial.java
@@ -1,7 +1,10 @@
 package com.jabirdeveloper.tinderswipe;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import com.bumptech.glide.Glide;
 public class rightTutorial extends AppCompatActivity {
 
     ImageView imageView;
+    Button change;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,17 @@ public class rightTutorial extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageview);
 
+        change = findViewById(R.id.begin);
+
+
+
         // Adding the gif here using glide library
         //Glide.with(this).load(R.drawable.swiperight).into(imageView);
     }
+
+    public void tutorialToSong (View view){
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+    }
+
 }
