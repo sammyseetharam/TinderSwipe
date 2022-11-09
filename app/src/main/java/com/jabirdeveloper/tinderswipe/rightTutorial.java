9d08f58c887020015d62818f922825e929600440
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2022-11-09 11:03:48 -0600

    tutorial screen working and card displaying with proper background format

--- app/src/main/java/com/jabirdeveloper/tinderswipe/rightTutorial.java
+++ app/src/main/java/com/jabirdeveloper/tinderswipe/rightTutorial.java
@@ -19,7 +19,7 @@ public class rightTutorial extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_right_tutorial);
         imageView = findViewById(R.id.imageview);
 
         change = findViewById(R.id.begin);
